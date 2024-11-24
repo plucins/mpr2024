e584d82cd73d65eda92d67134889e2201da39ce9
Slawomir Plucinski <external.slawomir.plucinski@kuehne-nagel.com>
2024-11-24 18:29:46 +0100

    Load as Entity

--- src/main/java/pl/edu/pjwstk/mpr/model/Loan.java
+++ src/main/java/pl/edu/pjwstk/mpr/model/Loan.java
@@ -1,16 +1,32 @@
 package pl.edu.pjwstk.mpr.model;
 
 import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import pl.edu.pjwstk.mpr.model.enums.LoanStatus;
 
+@Entity
 public class Loan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private LocalDateTime loanStartDate;
     private LocalDateTime expectedLoanEndDate;
     private LocalDateTime actualLoanEndDate;
     private LoanStatus loanStatus;
+    @OneToOne
     private User loanTo;
+    @OneToOne
     private User employee;
+    @OneToOne
     private Book loanBook;
+
+    public Loan() {
+    }
 
     public Loan(User loanTo, User employee, Book loanBook) {
         this.loanStartDate = LocalDateTime.now();
